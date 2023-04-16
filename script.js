bdfa158881f7da5f85ab1ac4acab86a5e6edc882
Vaidy05 <92988611+Vaidy05@users.noreply.github.com>
2023-04-16 14:08:13 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,6 @@
 //your JS code here. If required.
-function val(){
-	let str = document.getElementById();
-}
+
 function remove(){
 let item = document.getElementById("colorSelect");
-item.style.display="none";
+item.remove(item.selectedIndex);
 }
